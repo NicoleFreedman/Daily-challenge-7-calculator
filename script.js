816165b7fb71acc90042cb8a18f92684cd36d989
Nicole <x.nicole.freedman@gmail.com>
2019-12-27 17:22:43 +0200

    Update

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let displayValue = document.getElementById("display");
 let resultShowed = false;
 let resultValue = "";
+let newOperator = false;
 
 function addOperator(operator) {
+	if(newOperator){
+		resultValue = displayValue.value;
+	}
 	if (!(resultValue.value == "")) {
 		resultValue += operator;
 		resultShowed = true;
@@ -23,14 +27,15 @@ function display(symbol) {
 }
 
 function result(){
-
 	displayValue.value = eval(resultValue);
 	resultShowed = true;
 	resultValue = "";
+	newOperator = true;
 }
 
 function clearAll() {
 	displayValue.value = "0";
+	resultValue = "";
 }
 
 function deleteOne() {
